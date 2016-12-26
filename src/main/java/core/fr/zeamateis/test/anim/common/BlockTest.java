e65e3277d2c051bda2f318ba511f447e214f1ffb
ZeAmateis <theamateis@gmail.com>
2016-12-26 15:59:30 +0100

    Clean up code

--- src/main/java/core/fr/zeamateis/test/anim/common/BlockTest.java
+++ src/main/java/core/fr/zeamateis/test/anim/common/BlockTest.java
@@ -27,11 +27,6 @@ public class BlockTest extends Block implements ITileEntityProvider
         return false;
     }
 
-    @Override
-    public boolean isFullCube(IBlockState state) {
-        return false;
-    }
-
     /**
      * The type of render function called. 3 for standard block models, 2 for
      * TESR's, 1 for liquids, -1 is no render
--- src/main/java/core/fr/zeamateis/test/package-info.java
+++ src/main/java/core/fr/zeamateis/test/package-info.java
@@ -4,9 +4,9 @@
  * Some Samples codes to animate a block or entity with CraftStudio software
  * <p>
  * Created with Forge v12.18.1.2018
- * 
+ *
  * @author ZeAmateis
- * 
+ *
  * @version 0.9
  */
 package fr.zeamateis.test;
