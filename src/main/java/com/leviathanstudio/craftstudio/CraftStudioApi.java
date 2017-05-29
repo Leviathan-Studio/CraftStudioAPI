20dfeb8203d8e5d8d6f73b10c2f6f272a5b147af
Phenix246 <florian.gomes246@gmail.com>
2017-05-29 20:14:25 +0200

    Upgrade network system

--- src/main/java/com/leviathanstudio/craftstudio/CraftStudioApi.java
+++ src/main/java/com/leviathanstudio/craftstudio/CraftStudioApi.java
@@ -1,8 +1,5 @@
 package com.leviathanstudio.craftstudio;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.leviathanstudio.craftstudio.client.json.CSReadedAnim;
 import com.leviathanstudio.craftstudio.client.json.CSReadedModel;
 import com.leviathanstudio.craftstudio.common.animation.AnimationHandler;
@@ -17,9 +14,14 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.common.registry.RegistryBuilder;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Main class of the CraftStudioApi
@@ -31,16 +33,19 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @Mod(modid = CraftStudioApi.API_ID, name = CraftStudioApi.NAME, version = "0.1-beta", updateJSON = "https://leviathan-studio.com/craftstudioapi/update.json", acceptedMinecraftVersions = "1.11.2")
 public class CraftStudioApi
 {
-    private static final Logger  LOGGER = LogManager.getLogger("CraftStudio");
-    public static final String   API_ID = "craftstudioapi";
-    static final String          NAME   = "CraftStudio API";
+    private static final Logger              LOGGER  = LogManager.getLogger("CraftStudio");
+    public static final String               API_ID  = "craftstudioapi";
+    static final String                      NAME    = "CraftStudio API";
+
+    public static final SimpleNetworkWrapper NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel(CraftStudioApi.API_ID);
 
     @SidedProxy(clientSide = "com.leviathanstudio.craftstudio.proxy.CSClientProxy", serverSide = "com.leviathanstudio.craftstudio.proxy.CSServerProxy")
-    private static CSCommonProxy proxy;
+    private static CSCommonProxy             proxy;
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
-    public static void createRegistries(RegistryEvent.NewRegistry event) {
+    public static void createRegistries(RegistryEvent.NewRegistry event)
+    {
         RegistryBuilder builder = new RegistryBuilder<CSReadedModel>();
         builder.setName(new ResourceLocation(CraftStudioApi.API_ID, "cs_models"));
         builder.setType(CSReadedModel.class);
@@ -55,22 +60,26 @@ public class CraftStudioApi
 
     @SubscribeEvent(priority = EventPriority.LOW)
     @SideOnly(Side.CLIENT)
-    public static void registerModels(RegistryEvent.Register<CSReadedModel> e) {
+    public static void registerModels(RegistryEvent.Register<CSReadedModel> e)
+    {
         CSRegistryHelper.loadModels();
     }
 
     @SubscribeEvent(priority = EventPriority.LOW)
     @SideOnly(Side.CLIENT)
-    public static void registerAnims(RegistryEvent.Register<CSReadedAnim> e) {
+    public static void registerAnims(RegistryEvent.Register<CSReadedAnim> e)
+    {
         CSRegistryHelper.loadAnims();
     }
 
     @EventHandler
-    void preInit(FMLPreInitializationEvent event) {
+    void preInit(FMLPreInitializationEvent event)
+    {
         CraftStudioApi.proxy.preInit(event);
     }
 
-    public static Logger getLogger() {
+    public static Logger getLogger()
+    {
         return CraftStudioApi.LOGGER;
     }
 
@@ -81,7 +90,8 @@ public class CraftStudioApi
      * @param animated
      *            Class whiches implements IAnimated (Entity or TileEntity)
      */
-    public static AnimationHandler getNewAnimationHandler(IAnimated animated) {
+    public static AnimationHandler getNewAnimationHandler(IAnimated animated)
+    {
         return CraftStudioApi.proxy.getNewAnimationHandler(animated);
 
     }
